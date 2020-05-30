aff0318c653a5649720cd17c77b1c55dac594ff6
Carmen Adriana Martinez Barbosa <carmenita@mbp-de-carmen.home>
2020-05-30 13:41:47 +0200

    Update of everything!

--- HybridSinCosFit/__init__.py
+++
@@ -1,1 +0,0 @@
-from .HybridSinCosFit import *
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, Extension
 
 setup(
-    name='HybridSinCosFit',
+    name='HOBIT',
     version='0.0.2',
-    description='an installable package for Hybrid fitting of Sne and Cosine trigonometrical functions',
-    packages=['HybridSinCosFit'],
+    description='an installable package for Hybrid fitting of Sine and Cosine functions',
+    packages=['HOBIT'],
     author='Carmen Adriana Martinez Barbosa, Jose Arturo Celis Gil',
     author_email='anamabo3@gmail.com, solocelis@gmail.com',
-    url='https://github.com/anamabo/CosFitSklearn'
+    url='https://github.com/anamabo/HOBIT'
 )
